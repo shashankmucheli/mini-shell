26e1eeb59cfec564cc5582e2dc0db472e1fbb870
shashankmucheli <shashank.venku@gmail.com>
2014-10-07 12:47:10 -0400

    playing with pipes

--- execvp.c
+++ execvp.c
@@ -10,12 +10,7 @@ void  parse(char *cmd, char **argv)
 	while (*cmd != '\0') {      
         while (*cmd == ' ' || *cmd == '\t' || *cmd == '\n')
 			*cmd++ = '\0';     
-        *argv++ = cmd;          
-	//temp = *(argv-1);
-	/*if(i = 0){
-		printf("temp value is: %s\n",*(argv-1));
-		i=1;
-	}*/
+        *argv++ = cmd;
         while (*cmd != '\0' && *cmd != ' ' && 
 				*cmd != '\t' && *cmd != '\n') 
             cmd++;             
@@ -37,6 +32,7 @@ void  execute(char **argv)
     pid_t pid;
     int status;
 	int ampersand_found = 0;
+	int pipe_found = 0;
 	
 	if (!*argv) return;
 
@@ -85,13 +81,31 @@ void  execute(char **argv)
 	}
 	/* else { */
 		int i = 0;
-		for (i = 0; argv[i]; i++)
+		for (i = 0; argv[i]; i++){
 			if (strcmp (argv[i], "&") == 0) {
 				fprintf (stderr, "Ampersand Found!!\n");
 				ampersand_found = 1;
 				argv[i] = (char *)NULL;
 			}
-	/* } */
+			
+			if (strcmp (argv[i], "|") == 0) {
+				fprintf (stderr, "Pipe Found!!\n");
+				pipe_found = 1;
+				if(pipe_found == 1){
+					char **p_ptr;
+					*p_ptr = argv[1+1];
+					printf("%s", &p_ptr);
+					/*while(argv[i] != "\0"){
+						printf("%c", &argv[i]);
+						i++;
+					}*/
+					exit(0);
+				}
+				/*argv[i] = (char *)NULL;
+				char* p_ptr = argv[1+i];
+				printf("%c", *p_ptr);*/
+			}
+		}
 	
 	
 	if (strcmp(*argv, "exit") == 0){
@@ -99,17 +113,17 @@ void  execute(char **argv)
 	}
 	else if ((strcmp(*argv, "cd")) == 0) {
 		if ((chdir(argv[1])) != 0)
-			printf("Cannot change directory\n");
+			fprintf (stderr, "ERR: Cannot change directory!!\n");
     }
 	else{
 		pid = fork();
 		if (pid < 0) {     
-			printf("ERR: forking child process failed\n");
+			fprintf (stderr, "ERR: forking child process failed!!\n");
 			exit(1);
 		}
 		else if (pid == 0) {
 			if (execvp(*argv, argv) < 0) {
-				printf("ERR: exec failed\n");
+				fprintf (stderr, "ERR: exec failed!!\n");
 				exit(1);
 			}
 		}
